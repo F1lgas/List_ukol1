d05aae41d50d667154312b0884ffdc355469fcee
Filip Iran <iranf.03@spst.cz>
2022-04-28 17:58:57 +0200

    v1.0

--- List_ukol1/Form1.Designer.cs
+++ List_ukol1/Form1.Designer.cs
@@ -28,13 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generovat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Zadej N:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(101, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(178, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Zobrazit info";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(304, 25);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Výpis listu:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(301, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Výpis listu bez duplicit:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(435, 136);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "List Úkol1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- List_ukol1/Form1.cs
+++ List_ukol1/Form1.cs
@@ -12,9 +12,107 @@ namespace List_ukol1
 {
     public partial class Form1 : Form
     {
+        Random rnd = new Random();
+        List<int> cisla = new List<int>();
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private bool Prvocislo(int cislo)
+        {
+            double cislo2 = cislo / 2;
+            bool prvocislo = true;
+            for (int i = 2; i <= cislo2; i++)
+            {
+                if (cislo % i == 0)
+                {
+                    prvocislo = false;
+                }
+            }
+
+            if (prvocislo)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void Vypis(List<int> list, ListBox listbox)
+        {
+            listbox.Items.Clear();
+            listBox2.Items.Clear();
+
+            foreach (int item in list)
+            {
+                listbox.Items.Add(item);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cisla.Clear();
+
+            for (int i = 0; i < Convert.ToInt32(textBox1.Text); i++)
+            {
+                int cislo = rnd.Next(19, 31);
+                cisla.Add(cislo);
+            }
+
+            Vypis(cisla, listBox1);
+
+            foreach (int cislo in cisla)
+            {
+                if (listBox2.Items.Contains(cislo) == false)
+                {
+                    listBox2.Items.Add(cislo);
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int max = cisla.Max();
+            int prvnimax = 1;
+            int n = Convert.ToInt32(textBox1.Text);
+
+            for (int i = 0; i < n; i++)
+            {
+                if (cisla[i] == max)
+                {
+                    prvnimax = i;
+                    break;
+                }
+            }
+
+            bool je = false;
+            int prvcislo = 0;
+
+            foreach (int cislo in cisla)
+            {
+                if (Prvocislo(cislo) == true)
+                {
+                    je = true;
+                    prvcislo = cislo;
+                    break;
+                }
+            }
+
+            int pozicemax = cisla.LastIndexOf(max);
+
+            if (je == true)
+            {
+                MessageBox.Show("Pořadí prvního největšího čísla je " + (prvnimax + 1) + " a pořadí posledního je " + (pozicemax + 1) + Environment.NewLine + "Aritmetrický průměr prvků v listu je " + cisla.Average() + Environment.NewLine + "První prvočíslo je " + prvcislo);
+            }
+            else
+            {
+                MessageBox.Show("Pořadí prvního největšího čísla je " + (prvnimax + 1) + " a pořadí posledního je " + (pozicemax + 1) + Environment.NewLine + "Aritmetrický průměr prvků v listu je " + cisla.Average() + Environment.NewLine + "V listu nejsou žádná prvočísla");
+            }
         }
     }
 }
